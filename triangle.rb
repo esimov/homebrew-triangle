52a3325492a14c1308695a5d2c4af939324d8450
Endre Simo <esimovaq@yahoo.com>
2018-02-13 06:49:13 +0200

    Update hash

--- triangle.rb
+++ triangle.rb
@@ -4,7 +4,7 @@ class Triangle < Formula
   desc "Delaunay image triangulator library"
   homepage "https://github.com/esimov/triangle"
   url "#{homepage}/releases/download/v#{TRIANGLE_VERSION}/triangle-#{TRIANGLE_VERSION}-darwin-amd64.zip"
-  sha256 "ebb7cab087b5f2a8ffb61596c31642d288c6ced86585d7295bc8a8feff8a7cc9"
+  sha256 "eed2e428c94cd480e29044e01d25cdcced879347e0f6ad6f41f8dd47bc22aec8"
 
   depends_on "go" => :build
 
